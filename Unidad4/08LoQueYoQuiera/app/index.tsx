686d4423d6e318baa2c06cf374c78a28385f6a17
pabloiglesiasprieto <pablo.iglesias@iesnervion.es>
2025-10-21 11:06:31 +0200

    Tema 5

--- Unidad4/08LoQueYoQuiera/app/index.tsx
+++ Unidad4/08LoQueYoQuiera/app/index.tsx
@@ -1,5 +1,50 @@
-import View from "./src/View/View";
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-export default function Index() {
-  return <View />;
-}
+const RandomColorBox = () => {
+  const [color, setColor] = useState(getRandomColor());
+
+  function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue})`;
+  }
+
+  const handlePress = () => {
+    setColor(getRandomColor());
+  };
+
+  return (
+    <View style={[styles.container, { backgroundColor: color }]}>
+      <Text style={styles.text}>Color actual: {color}</Text>
+      <TouchableOpacity style={styles.button} onPress={handlePress}>
+        <Text style={styles.buttonText}>Cambiar color</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
+
+export default RandomColorBox;
